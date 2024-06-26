8b6c18a399d7c31506cdb8f0b2c9fd580392b75f
Ashwani Kumar <106174772+uk07developer@users.noreply.github.com>
2024-06-26 22:09:08 +0530

    Update DB_Insert.php

--- DB_Insert.php
+++ DB_Insert.php
@@ -1,1 +1,15 @@
+<?php
+include "DB_Connection.php";
 
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
